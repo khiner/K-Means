110be22338d6bfb367a6ea265fad75611f7ba4bc
Karl Hiner <karl.hiner@gmail.com>
2012-03-06 00:05:22 -0800

    cleanup

--- KMeans.py
+++ KMeans.py
@@ -2,37 +2,35 @@ from argparse import ArgumentParser
 import numpy as np
 
 class KMeans:
-    def __init__(self, k, trainPath):
-        self.k = k
+    def __init__(self, k):
+        self.k = k        
+        
+    def train(self, trainPath):
         data = np.loadtxt(trainPath, delimiter=',')
-        self.classes = data[:,0]
-        self.classes = self.classes.astype(int)
+        classes = data[:,0].astype(int)
         # remove classes - don't want to cheat :)
-        self.data = data[:,1:]
-        self.nData = self.data[:,0].size
-        self.nFeatures = self.data[0,:].size
+        featureData = data[:,1:]
+        nData = featureData[:,0].size
+        nFeatures = featureData[0,:].size
         # k centers, each with n features
-        self.centers = np.empty((self.k,self.nFeatures))
-        self.mostFrequentClass = np.empty(self.k, dtype='int')        
-        
-    def train(self):
-        self.initClusters()
-        while not self.learn():
+        self.centers = np.empty((self.k, nFeatures))
+        self.mostFrequentClass = np.empty(self.k, dtype='int')
+        self.initClusters(featureData, nFeatures)
+        while not self.learn(featureData, nData):
             pass
-        self.labelCenters()
+        self.labelCenters(classes)
         
-    def initClusters(self):
+    def initClusters(self, data, nFeatures):
         # arrays of mins and maxs for each feature
-        mins = np.amin(self.data, axis=0)
-        maxs = np.amax(self.data, axis=0)
+        mins = np.amin(data, axis=0)
+        maxs = np.amax(data, axis=0)
         
         # init each center with random values in the range of the features
         for i in range(self.k):
             # mins + (array of rands(0,1))*ranges
-            self.centers[i,:] = mins + np.random.random(self.nFeatures)* \
-                                (maxs - mins)
+            self.centers[i,:] = mins + np.random.random(nFeatures)*(maxs - mins)
         
-    def learn(self):
+    def learn(self, data, nData):
         """Compute distances, find the closest cluster, and update
         the cluster centers"""
 
@@ -40,17 +38,17 @@ class KMeans:
         oldCenters = np.copy(self.centers)
 
         # compute distances
-        distances = self.computeDistances(self.data, self.nData)
+        distances = self.computeDistances(data, nData)
         
         # find the closest cluster
         self.cluster = distances.argmin(axis=0)
-        self.cluster = np.transpose(self.cluster*np.ones((1,self.nData), dtype='int'))
+        self.cluster = np.transpose(self.cluster*np.ones((1,nData), dtype='int'))
 
         # update the cluster centers
         for i in range(self.k):
             thisCluster = np.where(self.cluster == i, 1, 0)
             if np.sum(thisCluster) > 0:
-                self.centers[i,:] = np.sum(self.data*thisCluster, axis=0)/np.sum(thisCluster)
+                self.centers[i,:] = np.sum(data*thisCluster, axis=0)/np.sum(thisCluster)
 
         centerDiff = oldCenters - self.centers
         nUnequal = centerDiff[np.where(centerDiff != 0)].size
@@ -64,11 +62,10 @@ class KMeans:
             distances[i] = np.sum((data - self.centers[i,:])**2, axis=1)
         return distances
         
-    def labelCenters(self):
+    def labelCenters(self, classes):
         for i in range(self.k):
             thisCluster = np.where(self.cluster == i, 1, 0)
-            clusterClasses = np.transpose(thisCluster)*self.classes
-            clusterClasses = clusterClasses[0,:]
+            clusterClasses = (np.transpose(thisCluster)*classes)[0,:]
             counts = np.bincount(clusterClasses)
             # find the bin with the highest count, excluding 0
             self.mostFrequentClass[i] = np.argmax(counts[1:]) + 1
@@ -77,13 +74,12 @@ class KMeans:
 
     def test(self, testPath):
         data = np.loadtxt(testPath, delimiter=',')
-        testClasses = data[:,0]
-        testClasses = testClasses.astype(int)
+        testClasses = data[:,0].astype(int)
         testData = data[:,1:]
         nTestData = testData[:,0].size
         distances = self.computeDistances(testData, nTestData)
-        cluster = distances.argmin(axis=0)
-        predictedClasses = self.mostFrequentClass[cluster]
+        self.cluster = distances.argmin(axis=0)
+        predictedClasses = self.mostFrequentClass[self.cluster]
         classDiff = testClasses - predictedClasses
         nCorrect = classDiff[np.where(classDiff == 0)].size
         cohesion = self.computeCohesion(testData)
@@ -99,7 +95,7 @@ class KMeans:
 parser = ArgumentParser()
 parser.add_argument('-k', type=int, default=3, help='number of clusters')
 args = parser.parse_args()
-kMeans = KMeans(args.k, 'data/wine.train')
-kMeans.train()
+kMeans = KMeans(args.k)
+kMeans.train('data/wine.train')
 kMeans.test('data/wine.test')
 
