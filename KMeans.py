fixed regression bug where self.centers was not being assigned at all, and normalizing across features for improved accuracy

KMeans.py
@@ -12,6 +12,7 @@ class KMeans:
         featureData = data[:,1:]
         nData = featureData[:,0].size
         nFeatures = featureData[0,:].size
+        featureData /= np.max(featureData, axis=0)
         # k centers, each with n features
         self.centers = np.empty((self.k, nFeatures))
         self.mostFrequentClass = np.empty(self.k, dtype='int')
@@ -19,6 +20,24 @@ class KMeans:
         while not self.learn(featureData, nData):
             pass
         self.labelCenters(classes)
+        cohesion = self.cohesion(data)
+        separation = self.separation(data)        
+        print 'Cohesion:', cohesion
+        print 'Separation:', separation
+        
+    def test(self, testPath):
+        data = np.loadtxt(testPath, delimiter=',')
+        testClasses = data[:,0].astype(int)
+        testData = data[:,1:]
+        nTestData = testData[:,0].size
+        nFeatures = testData[0,:].size
+        testData /= np.max(testData, axis=0)
+        distances = self.computeDistances(testData, nTestData)
+        self.cluster = distances.argmin(axis=0)
+        predictedClasses = self.mostFrequentClass[self.cluster]
+        classDiff = testClasses - predictedClasses
+        nCorrect = classDiff[np.where(classDiff == 0)].size
+        print "accuracy", float(nCorrect)/float(testClasses.size)
         
     def initClusters(self, data, nFeatures):
         # arrays of mins and maxs for each feature
@@ -48,7 +67,7 @@ class KMeans:
             thisCluster = data[np.where(self.cluster == i)]
             clusterSize = thisCluster[:,0].size
             if clusterSize > 0:
-                testCenter = np.sum(thisCluster, axis=0)/clusterSize
+                self.centers[i] = np.sum(thisCluster, axis=0)/clusterSize
                 
         centerDiff = oldCenters - self.centers
         nUnequal = centerDiff[np.where(centerDiff != 0)].size
@@ -68,48 +87,49 @@ class KMeans:
             clusterClasses = (np.transpose(thisCluster)*classes)
             counts = np.bincount(clusterClasses)
             # find the bin with the highest count, excluding 0
-            self.mostFrequentClass[i] = np.argmax(counts[1:]) + 1
+            print counts[1:]
+            print counts[1:].size
+            if counts[1:].size:
+                self.mostFrequentClass[i] = np.argmax(counts[1:]) + 1
+            else:
+                self.mostFrequentClass[i] = 0
 
         print self.mostFrequentClass
 
-    def test(self, testPath):
-        data = np.loadtxt(testPath, delimiter=',')
-        testClasses = data[:,0].astype(int)
-        testData = data[:,1:]
-        nTestData = testData[:,0].size
-        distances = self.computeDistances(testData, nTestData)
-        self.cluster = distances.argmin(axis=0)
-        predictedClasses = self.mostFrequentClass[self.cluster]
-        classDiff = testClasses - predictedClasses
-        nCorrect = classDiff[np.where(classDiff == 0)].size
-        cohesion, separation = self.cohesionSeparation(testData)
-        print cohesion
-        print separation
-        print "accuracy", float(nCorrect)/float(testClasses.size)
-
-    def cohesionSeparation(self, data):
-        cohesion = np.zeros(self.k)
-        separation = np.zeros((self.k, self.k))
+    def cohesion(self, data):
+        cohesion = np.zeros(self.k)        
         
-        # currently cohesion is across all data.  need for each cluster
         for i in range(self.k):
             thisCluster = data[np.where(self.cluster == i)]
             thisClusterSize = thisCluster[:,0].size
             thisCohesion = np.zeros(thisClusterSize**2)
+            N = 0
             for j in xrange(thisClusterSize):
                 for k in xrange(j + 1, thisClusterSize):
+                    N += 1
                     thisCohesion[j*k + k] = np.sum((thisCluster[j] - thisCluster[k])**2)
-            cohesion[i] = 1/np.sum(thisCohesion)
+            cohesion[i] = N/np.sum(thisCohesion)
+
+        return np.average(cohesion)
+
+    def separation(self, data):
+        separation = np.zeros(self.k*self.k)
+        C = 0
+        # currently cohesion is across all data.  need for each cluster
+        for i in range(self.k):
+            thisCluster = data[np.where(self.cluster == i)]
+            thisClusterSize = thisCluster[:,0].size
             for j in range(i + 1, self.k):
                 thatCluster = data[np.where(self.cluster == j)]
                 thatClusterSize = thatCluster[:,0].size
                 thisSeparation = np.zeros(thisClusterSize*thatClusterSize)
                 for m in xrange(thisClusterSize):
-                    for n in xrange(m + 1, thatClusterSize):
+                    for n in xrange(thatClusterSize):
                         thisSeparation[m*n + n] = np.sum((thisCluster[m] - thatCluster[n])**2)
-                separation[i, j] = np.sum(thisSeparation)
-        
-        return (cohesion, separation)
+                separation[C] = np.sum(thisSeparation)/(thisClusterSize*thatClusterSize)
+                C += 1
+
+        return np.average(separation[:C])
         
 parser = ArgumentParser()
 parser.add_argument('-k', type=int, default=3, help='number of clusters')